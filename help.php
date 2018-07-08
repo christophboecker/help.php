c4c66dc8b7ff20627b4c3b6a29356ae622132cbf
christophboecker <christoph_boecker@t-online.de>
2018-07-08 15:49:32 +0200

    Add files via upload

--- help.php
+++ help.php
@@ -10,44 +10,93 @@
  *  file that was distributed with this source code.
  *
  *  ------------------------------------------------------------------------------------------------
+ *  package.yml
  *
+ *          ...
+ *          help:
+ *              mode: docs | readme         # default: docs
+ *              fallback: «locale»          # primary fallback-language
+ *              content: main_intro.md      # default-page for content (docs only)
+ *              navigation: main_navi.md    # default-page for navigation (docs only)
+ *              markdown_break_enabled: 0|1 # default is false (parsedown-standard)
+ *              title:                      # optional page title (try to avoid)
  *
+ * «addon/docs
+ *      README.«locale».md          # language specific README (readme-mode)
+ *      «file(s)»                   # language independent (image-)files (docs-mode)
+ *      «locale»                    # language specific files (docs-mode)
+ *          main_intro.md
+ *          main_navi.md
+ *          «file(s)»
+ *
+
  */
 
 // label for specific URL-parameters
 const URL_FILE = 'doc_file';
 const URL_IMAGE = 'doc_img';
 
-// parameters for package.yml
+// parameters in package.yml
 const YML_SECTION = 'help';
 const YML_MODE = 'mode';
 const YML_LANG = 'fallback';
 const YML_NAVI = 'navigation';
 const YML_CONT = 'content';
+const YML_MDBE = 'markdown_break_enabled';
+const YML_HEAD = 'title';
 
 // others
 const PATH_DOC = 'docs/';
 const DEF_MODE = 'readme';
 const DOC_MODE = 'docs';
-const DEF_LANG = '';
-const DEF_NAVI = '_navi.md';
-const DEF_CONT = '_intro.md';
+const DEF_NAVI = 'main_navi.md';
+const DEF_CONT = 'main_intro.md';
+const DEF_MDBE = '0';
+const MDBE_ON = '1';
+const DEF_HEAD = '';
 
+
+//## helpfile
 // read defaults and configuration
 $defaults = array_merge (
-    [YML_MODE=>DEF_MODE,YML_LANG=>DEF_LANG,YML_NAVI=>DEF_NAVI,YML_CONT=>DEF_CONT],
+    [YML_MODE=>DEF_MODE,YML_LANG=>rex_i18n::getLocale(),YML_NAVI=>DEF_NAVI,YML_CONT=>DEF_CONT,YML_MDBE=>DEF_MDBE,YML_HEAD=>DEF_HEAD],
     (array)$this->getProperty( YML_SECTION, [] )
 );
 
+// override defaults with page-specific configuration
+$page = rex_request('page','string');
+
+// page = addon/.../...
+if( $this->getName() == rex_be_controller::getCurrentPagePart(1) ) {
+    $i = 1;
+    $pageDefaults = (array)$this->getProperty('page');
+    while ( ($x=rex_be_controller::getCurrentPagePart(++$i)) !== null) {
+        $pageDefaults = (array)$pageDefaults['subpages'][$x];
+    }
+// or somewhere else in the "pages"
+} elseif( isset( $this->getProperty('pages')[$page] )) {
+    $pageDefaults = (array)$this->getProperty('pages')[$page];
+} else {
+    $pageDefaults = [];
+}
+$defaults = array_merge (
+    $defaults,
+    isset($pageDefaults[YML_SECTION]) ? (array)$pageDefaults[YML_SECTION] : []
+);
+
+//## helpfile
 // language and fallback
-$language = rex::getProperty('lang');
+$language = $defaults[YML_LANG];
 $fallback = array_merge( [$defaults[YML_LANG]], rex::getProperty('lang_fallback', []) );
 $fallback = array_values(array_unique(array_diff( $fallback, [$language,''] )));
 
+//## helpfile
 // directory names
 $docPath = $this->getPath( PATH_DOC );
 $langPath = $docPath . "$language/";
+$hasDocDir = is_dir( $docPath );
 
+//## helpfile
 // if image-call: send image
 // relevant only in docMode -> assure language-fallback
 if ( ($image = rex_request( URL_IMAGE, 'string')) != '' )
@@ -58,9 +107,10 @@ if ( ($image = rex_request( URL_IMAGE, 'string')) != '' )
         exit;
     } else {
         foreach( $fallback as $v ) {
-            if( !is_file($imagefile="$docPath$v/$image" ) ) continue;
-            rex_response::sendFile( $imagefile, mime_content_type( $imagefile ) );
-            exit;
+            if( is_file($imagefile="$docPath$v/$image" ) ) {
+                rex_response::sendFile( $imagefile, mime_content_type( $imagefile ) );
+                exit;
+            }
         }
     }
     header( 'HTTP/1.1 Not Found' );
@@ -70,10 +120,6 @@ if ( ($image = rex_request( URL_IMAGE, 'string')) != '' )
 // set File_not_found-Message
 $file_not_found = $this->i18n('docs_not_found');
 
-// detect operation modus: pure readme vs. multifile-documentation
-// no docPath? switch to readme-Mode
-$docMode = $defaults[YML_MODE] === DOC_MODE && is_dir( $docPath );
-
 // generate the fixed part of the link-url
 parse_str($_SERVER['QUERY_STRING'], $url);
 $url = array_diff_key( $url, [URL_FILE=>0,URL_IMAGE=>0] );
@@ -82,8 +128,11 @@ $urlDoc = $url . '&' . URL_FILE . '=';
 $urlImg = $url . '&' . URL_IMAGE . '=';
 
 
+// detect operation modus: pure readme vs. multifile-documentation
+// no docPath? switch to readme-Mode
+
 // Documentation-Mode ------------------------------------------------------------------------------
-if( $docMode )
+if( $defaults[YML_MODE] === DOC_MODE && $hasDocDir )
 {
     // correct language-directory
     // if none found: keep existing value
@@ -95,26 +144,9 @@ if( $docMode )
         }
     }
 
-    // get filename for navigation and content;
-    $page = rex_request('page','string');
-    // page = addon/.../...
-    if( $this->getName() == rex_be_controller::getCurrentPagePart(1) ) {
-        $i = 1;
-        $pageDefaults = (array)$this->getProperty('page');
-        while ( ($x=rex_be_controller::getCurrentPagePart(++$i)) !== null) {
-            $pageDefaults = (array)$pageDefaults['subpages'][$x];
-        }
-    // or somewhere else in the "pages"
-    } elseif( isset( $this->getProperty('pages')[$page] )) {
-        $pageDefaults = (array)$this->getProperty('pages')[$page];
-    } else {
-        $pageDefaults = [];
-    }
-
-    if( !isset($pageDefaults[YML_SECTION]) ) $pageDefaults[YML_SECTION] = [];
-    $pageDefaults = array_merge( $defaults,$pageDefaults[YML_SECTION] );
-    $navigationFile = $pageDefaults[YML_NAVI];
-    $contentFile = rex_request( URL_FILE, 'string', $pageDefaults[YML_CONT] );
+    // get filenames
+    $navigationFile = $defaults[YML_NAVI];
+    $contentFile = rex_request( URL_FILE, 'string', $defaults[YML_CONT] );
 
     // read content- and navigation-file
     $content = rex_file::get( "$langPath$contentFile" );                // addon/docs/de_de/xy.md
@@ -123,6 +155,8 @@ if( $docMode )
     $navigation = rex_file::get( "$langPath$navigationFile" );
     if( !$navigation ) $navigation = rex_file::get( "$docPath$navigationFile" );
 
+    // Mark currently selected Chapter in the Menü
+    $navigation = preg_replace( '/\[(.*)\]\(('.preg_quote ($contentFile).'.*)\)/','[$1]($2){.bg-primary}',$navigation);
 
     // set system-internal links
     //  1) get system-internal filenames
@@ -140,6 +174,7 @@ if( $docMode )
         $navigation = preg_replace_callback( $search, $replace, $navigation );
     }
 
+
 // Readme-Mode ------------------------------------------------------------------------------
 } else {
     // for hooks
@@ -152,17 +187,24 @@ if( $docMode )
         foreach( $fallback as $v ) {
             if( is_file( $f="{$docPath}README.$v.md" ) ) {
                 $filename = $f;
+                $language = $v;
                 break;
             }
         }
     }
     $content = rex_file::get( $filename );
     // treat empty file as "not found"
-    if( !$content ) $content = $file_not_found . " $filename";
+    if( $content ) {
+        $content = PHP_EOL.$content;
+    } else {
+        $content = $file_not_found . " $filename";
+    }
 
     // Identify headlines of Level 2 and 3 from content-file including preceeding hooks and make $match handy
-    preg_match_all( '/((<a name="(.*)"><\/a>\s*[\r\n]*)*)(#{2,3})\s(.*)/', $content, $match, PREG_OFFSET_CAPTURE );
+    preg_match_all( '/[\r\n]((<a name="(.*)"><\/a>\s*[\r\n]*)*)(#{2,3})\s(.*)/', $content, $match, PREG_OFFSET_CAPTURE );
     foreach( $match[0] as $k=>$v ) {
+        $level = strlen($match[4][$k][0]);
+        if( $level < 2 || $level > 3) ;
         $match[0][$k] = ['level'=>strlen($match[4][$k][0]), 'titel'=>$match[5][$k][0], 'offset'=>$v[1]];
     }
     $match = $match[0];
@@ -186,16 +228,22 @@ if( $docMode )
         $hook['#'.$v[0]] = key( $offset );
     }
 
+    // section/chapter requested by URL
+    $section = rex_request( URL_FILE, 'int', 0 );
+
     // Generate navigation
     // only use headlines of level 2 and 3
+    // Mark currently selected Chapter in the Menü
     $matchNav = array_filter( $match, function($m){return $m['level']==2 || $m['level']==3; });
     $navigation = '';
     foreach( $matchNav as $k=>$v ) {
-        $navigation .= str_repeat( " ", $v['level']-1 ) . "- [{$v['titel']}]($urlDoc$k#$marker$k)" . PHP_EOL;
+        $navigation .= str_repeat( " ", $v['level']-1 ) . "- [{$v['titel']}]($urlDoc$k#$marker$k)"
+            .( $k == $section ? '{.bg-primary}': '' )
+            . PHP_EOL;
     }
 
     // identify and clip out active Section
-    if( ($section = rex_request( URL_FILE, 'string', 0 )) )  {
+    if( $section )  {
         while ( $match[$section]['level'] > 2 && $section > 0 ) $section--;
     } else {
         $section = 0;
@@ -211,7 +259,7 @@ if( $docMode )
         $content = substr_replace( $content, '<a name="'.$marker.$k.'"></a>'.PHP_EOL, $v['offset']-$clipOffset, 0);
     }
 
-    // correct internal references to outside-chapters
+    // correct internal references to chapters outside the clipped one
     preg_match_all( '/\[.*\]\((#.*)\)/', $content, $links, PREG_OFFSET_CAPTURE );
     foreach( array_reverse($links[1],true) as $k=>$v )
     {
@@ -220,17 +268,33 @@ if( $docMode )
         }
     }
 
+    // correct image-links to doc-files
+    // detect filenames in the docPath-Dir
+    // correct image-links for detected filenames
+    if( $hasDocDir ) {
+        $iterator = rex_finder::factory( $docPath );
+        $replace = '![$1]('.$urlImg.'$2)';
+        foreach( $iterator as $v) {
+            $search = '/!\[(.*)\]\(('.preg_quote ( $v->getFilename() ).'.*)\)/';
+            $content = preg_replace( $search, $replace, $content );
+        }
+    }
 }
 
 // Output ------------------------------------------------------------------------------------------
 
 // parse markdown
 $parser = new ParsedownExtra();
+if( YML_MDBE == MDBE_ON ) $parser->setBreaksEnabled(true);
 $content = $parser->text($content);
 $navigation = $parser->text($navigation);
 unset( $parser );
 
-echo '<section class="rex-docs rex-yform-docs"><div class="row">';
+if( $defaults[YML_HEAD] ) {
+    echo rex_view::title(rex_i18n::translate($defaults[YML_HEAD]));
+}
+
+echo '<section class="rex-yform-docs"><div class="row">';
 
 $fragment = new rex_fragment();
 $fragment->setVar('title', $this->i18n('docs_content'), false );
@@ -240,13 +304,15 @@ if( $navigation )        {
     $fragment = new rex_fragment();
     $fragment->setVar('title', $this->i18n('docs_navigation'), false);
     $fragment->setVar('body', $navigation, false);
-    echo '<div class="col-md-4 rex-docs yform-docs-navi">'.$fragment->parse('core/page/section.php').'</div>';
-    echo '<div class="col-md-8 docs-content yform-docs-content">'.$content.'</div>';
+    echo '<div class="col-md-4 yform-docs-navi">'.$fragment->parse('core/page/section.php').'</div>';
+    echo '<div class="col-md-8 yform-docs-content">'.$content.'</div>';
 } else {
-    echo '<div class="col-md-12 docs-content yform-docs-content">'.$content.'</div>';
+    echo '<div class="col-md-12 yform-docs-content">'.$content.'</div>';
 }
 echo '</div></section>';
-?>
+//echo '<link rel="stylesheet" type="text/css" media="all" href="../assets/addons/yform/plugins/docs/docs.css?buster=1530520815">';
+echo '<style> .rex-yform-docs img {max-width:100%;}</style>';
+/* ?>
 <style>
 .rex-yform-docs h1 {
     font-size: 22px;
@@ -362,3 +428,4 @@ font-family: FontAwesome;
     display: none;
 }
 </style>
+<?php /**/
